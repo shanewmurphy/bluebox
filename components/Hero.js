18c29b624960cfa09feb2c22f064b6bb946c1c4f
shanewmurphy <shn.murphy@gmail.com>
2023-08-01 10:36:03 +0100

    Mobile Text styling & padding

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div>
-      <div className="w-11/12 mx-auto mt-6 bg-gradient-to-r from-gradientBlue to-gradientWhite lg:p-16 md:p-12 sm:p-6 rounded-xl antialiased">
+      <div className="w-11/12 mx-auto mt-6 bg-gradient-to-r from-gradientBlue to-gradientWhite lg:p-16 md:p-12 sm:p-8 sm:pb-14 rounded-xl antialiased">
         <div className="lg:w-7/12 md:w-7/12 sm:9/12 mx-auto">
           <div>
             <Image
@@ -21,7 +21,7 @@ export default function Hero() {
           <h4 className="lg:text-4xl md:text-3xl sm:text-base font-semibold text-center text-gray mt-2">
             Your Trusted Storage Solution
           </h4>
-          <div className="lg:w-80 md:w-60 sm:w-52 mx-auto mt-8">
+          <div className="lg:w-80 md:w-60 sm:w-56 mx-auto mt-8">
             <button className="bg-blue text-white lg:text-2xl md:text-2xl sm:text-base font-semibold w-full rounded-md py-4">
               Select Your Container
             </button>
