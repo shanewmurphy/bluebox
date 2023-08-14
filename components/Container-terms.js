49eb21df6353972b3f60de442eee74ad6dd039ea
shanewmurphy <shn.murphy@gmail.com>
2023-08-14 19:29:07 +0100

    20ft page | edits 8ft container page

--- components/Container-terms.js
+++ components/Container-terms.js
@@ -1,6 +1,6 @@
 export default function ContainerTerms() {
   return (
-    <div className="w-11/12 mx-auto bg-bkgGray antialiased lg:p-16 md:p-6 sm:p-6 lg:mt-40 lg:mb-40">
+    <div className="w-11/12 mx-auto rounded-xl bg-bkgGray antialiased lg:py-12 md:py-8 lg:px-12 md:px-8 sm:py-8 sm:px-4 lg:my-60 md:my-32 sm:my-24">
       <div>
         <h3 className="text-center text-black lg:text-6xl md:text-5xl sm:text-4xl font-extrabold mb-6">
           Container Rental Terms & Conditions
@@ -9,38 +9,38 @@ export default function ContainerTerms() {
       <div class="grid lg:grid-cols-2 lg:gap-x-16 md:gap-x-8 lg:gap-y-8 md:gap-y-8 sm:gap-y-8">
         <div>
           <h6 className="text-3xl font-bold text-blue mb-1">Rental Period</h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs leading-relaxed font-medium text-black mb-2">
             The rental period commences on the agreed-upon start date and
             terminates on the specified end date as outlined in the rental
             agreement.
           </p>
-          <p className="text-sm font-medium text-black">
+          <p className="lg:text-sm md:text-sm sm:text-xs leading-relaxed font-medium text-black">
             Rental extensions may be available upon request and subject to
             availability.
           </p>
         </div>
         <div>
           <h6 className="text-3xl font-bold text-blue mb-1">Payment Terms</h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mb-2">
             Renter agrees to pay the rental fee as specified in the rental
             agreement.
           </p>
-          <p className="text-sm font-medium text-black">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black">
             Payments are due on a weekly/monthly/quarterly basis and should be
             made by the agreed-upon payment method.
           </p>
-          <p className="text-sm font-medium text-black mt-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mt-2">
             Late payments may incur additional fees or penalties as outlined in
             the rental agreement.
           </p>
         </div>
         <div>
           <h6 className="text-3xl font-bold text-blue mb-1">Access and Use</h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mb-2">
             Renter is granted exclusive access to the rented storage container
             for the duration of the rental period.
           </p>
-          <p className="text-sm font-medium text-black">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black">
             The container is to be used solely for storage purposes and may not
             be sublet or shared without prior written consent from the Company.
           </p>
@@ -49,7 +49,7 @@ export default function ContainerTerms() {
           <h6 className="text-3xl font-bold text-blue mb-1">
             Prohibited Items
           </h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mb-2">
             Renter agrees not to store any illegal substances, hazardous
             materials, flammable items, perishable goods, live animals,
             valuables & irreplaceable items, illegal substances, firearms and
@@ -59,22 +59,22 @@ export default function ContainerTerms() {
         </div>
         <div>
           <h6 className="text-3xl font-bold text-blue mb-1">Liability</h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mb-2">
             The Company shall not be held liable for any loss, damage, or theft
             of Renter&apos;s stored belongings.
           </p>
-          <p className="text-sm font-medium text-black">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black">
             Renter is encouraged to secure appropriate insurance coverage for
             their stored items.
           </p>
         </div>
         <div>
           <h6 className="text-3xl font-bold text-blue mb-1">Termination</h6>
-          <p className="text-sm font-medium text-black mb-2">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black mb-2">
             Either party may terminate the contract by providing written notice
             within the specified notice period as outlined in the agreement.
           </p>
-          <p className="text-sm font-medium text-black">
+          <p className="lg:text-sm md:text-sm sm:text-xs font-medium text-black">
             Upon termination, Renter shall remove all belongings from the
             container and return it in the same condition as received, subject
             to a final inspection by the Company.
--- pages/20ft-container.js
+++ pages/20ft-container.js
@@ -1,42 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
+import twentyFtContainer from "../public/TwentyFtContainer.png";
+
 import PickUpService from "@/components/Pick-Up-Service";
 import Security from "@/components/Security";
 import ContainerTerms from "@/components/Container-terms";
 import FAQCommon from "@/components/FAQ/Common";
 import Testimonials from "@/components/Testimonials/Testimonials";
 
-export default function EightFTContainer() {
+export default function TwentyFtContainer() {
   return (
     <div>
       <div className="w-11/12 mx-auto mt-16 bg-bkgGray lg:py-12 lg:px-12 md:py-8 md:px-8 sm:py-4 sm:px-4 lg:rounded-2xl md:rounded-xl sm:rounded-md antialiased">
         <div className="flex lg:flex-row md:flex-col sm:flex-col lg:gap-16 md:gap-8 sm:gap-6">
           <div className="basis-2/5 rounded-md bg-white p-4">
             <Image
-              className="m-0 mx-auto lg:mt-10"
-              src="/8ft_container_Blue-min.png"
-              width={320}
-              height={248}
+              className="m-0 mx-auto lg:mt-20"
+              src={twentyFtContainer}
+              width={335}
+              height={193}
               alt="8ft container"
+              priority
             />
           </div>
           <div className="basis-3/5">
             <h3 className="lg:text-5xl md:text-5xl sm:text-4xl font-extrabold text-black">
-              8ft Storage Container
+              20ft Storage Container
             </h3>
             <h6 className="font-semibold text-base text-black">
-              Compact and Convenient
+              Spacious and Versatile
             </h6>
             <h5 className="lg:mt-4 md:mt-4 sm:mt-4 lg:text-2xl md:text-2xl sm:text-2xl font-bold text-black">
               Key Features
             </h5>
             <p className="font-semibold lg:text-base md:text-base sm:text-sm text-black">
-              Dimensions: The 8ft container measures dimensions Lenght: 2.44m X
-              Width: 2.20m X Height: 2.26m
+              Dimensions: The 20ft container measures dimensions Lenght: 5.90m X
+              Width: 2.35m X Height: 2.39m
             </p>
             <p className="mt-2 font-semibold lg:text-base md:text-base sm:text-sm text-black">
-              Storage Capacity: With a storage capacity of approximately hold
-              the contents of a studio apartment or a small office space
+              Storage Capacity: Spacious interior of the 20ft container offers
+              room to store the contents of a one-to-two-bedroom apartment or a
+              small office.
             </p>
             <p className="mt-2 font-semibold lg:text-base md:text-base sm:text-sm text-black">
               Security: Equipped with
@@ -46,7 +50,7 @@ export default function EightFTContainer() {
               >
                 security features
               </Link>
-              , our 8ft container provides a secure storage solution.
+              , our 20ft container provides a secure storage solution.
             </p>
             <div className="lg:mt-8 md:mt-8 sm:mt-12 sm:mb-8 lg:text-left md:text-center sm:text-center">
               <button className="lg:py-4 lg:px-16 md:py-4 md:px-16 sm:py-4 sm:px-12 sm:text-center bg-blue font-bold text-white text-2xl rounded-md antialiased">
@@ -68,25 +72,24 @@ export default function EightFTContainer() {
           </div>
           <div className="basis-3/5 antialiased">
             <h4 className="lg:text-5xl mb-2 md:text-4xl sm:text-4xl font-extrabold text-black">
-              8ft Container Suitability
+              20ft Container Suitability
             </h4>
             <p className="font-medium text-sm leading-relaxed text-gray lg:pr-16">
-              An 8ft storage container is a versatile solution for storing a
-              range of items. It is suitable for small to medium-sized furniture
-              pieces, such as chairs, tables, and compact appliances.
+              The 20ft container is designed to cater to a diverse range of
+              storage needs, making it an ideal choice for individuals,
+              businesses, and organisations. Whether you&apos;re a homeowner in
+              the midst of a move or renovation, a small business owner seeking
+              extra space for inventory, or a contractor requiring on-site
+              storage for tools and equipment, the 20ft container has you
+              covered.
             </p>
             <p className="mt-4 font-medium leading-relaxed text-sm text-gray lg:pr-16">
-              Personal belongings like clothing, books, and toys can be easily
-              stored in this container. It&apos;s also ideal for seasonal items
-              such as holiday decorations, sports equipment, camping gear, and
-              gardening tools.
-            </p>
-            <p className="mt-4 font-medium leading-relaxed text-sm text-gray lg:pr-16">
-              For small businesses, the 8ft container provides space for office
-              supplies, inventory, and equipment. Whether you&apos;re moving,
-              renovating, or need additional storage for your belongings, the
-              8ft container offers a compact and secure solution to meet your
-              needs.
+              It can accommodate the contents of a one-to-two-bedroom apartment
+              or house, including furniture, appliances, boxed items, and more.
+              For businesses, it offers ample room for office equipment,
+              inventory, filing cabinets, and even medium-sized machinery. Its
+              versatility makes it suitable for various industries, including
+              retail, event planning, education, and construction.
             </p>
           </div>
         </div>
